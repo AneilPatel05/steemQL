48d1ec8a20c53c815105247b7026670df93f542a
Luis Höfer <luis@superlumen.io>
2017-07-04 10:44:31 +0200

    Enable cors

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ const graphQLServer = express();
 graphQLServer.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
+graphQLServer.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 graphQLServer.listen(process.env.PORT || GRAPHQL_PORT, () =>
   console.log(
     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
