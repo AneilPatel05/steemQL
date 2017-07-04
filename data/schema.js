f61a67839750e9b782d363594850dbf771cc7120
Luis Höfer <luis@superlumen.io>
2017-07-04 22:43:36 +0200

    Add account history

--- data/schema.js
+++ data/schema.js
@@ -5,12 +5,14 @@ import DGP from "./globals/dgp.schema";
 import Tag from "./tag/tag.schema";
 import Post from "./post/post.schema";
 import User from "./user/user.schema";
+import UserHistory from "./user/userHistory.schema";
 import Mention from "./mentions/mention.schema";
 
 const rootSchema = `
   type Query {
     user(username: String!): User 
     users(users: [String]!, limit: Int): [User]
+    getUserHistory(username: String!, from: Int, limit: Int): String 
     mentions(username: String!): [Mention]
     getConfig: Config
     getDynamicGlobalProperties: DGP
@@ -28,7 +30,16 @@ const rootSchema = `
   }
 `;
 
-const typeDefs = [rootSchema, Config, DGP, Tag, Post, User, Mention];
+const typeDefs = [
+  rootSchema,
+  Config,
+  DGP,
+  Tag,
+  Post,
+  User,
+  UserHistory,
+  Mention
+];
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
--- data/user/user.resolver.js
+++ data/user/user.resolver.js
@@ -19,6 +19,20 @@ const UserResolvers = {
     async users(root, args) {
       const users = await steem.api.getAccounts(args.users);
       return users;
+    },
+
+    /**
+     * Get accounts history from steem.
+     * @param root
+     * @param args
+     * @return {Promise.<*>}
+     */
+    async getUserHistory(root, args) {
+      const { username, from = 50, limit = 25 } = args;
+      const history = await steem.api.getAccountHistory(username, from, limit);
+      console.log(JSON.stringify(history));
+      // Have to flatten
+      return JSON.stringify(history);
     }
   }
 };
