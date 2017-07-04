b0048e0d4081ffc08c4f56b4c7e52f755dbda11a
Luis Höfer <luis@superlumen.io>
2017-07-04 10:54:07 +0200

    Use cors package

--- server.js
+++ server.js
@@ -2,23 +2,17 @@ import express from "express";
 import { graphqlExpress, graphiqlExpress } from "graphql-server-express";
 import bodyParser from "body-parser";
 import schema from "./data/schema";
+import cors from "cors";
+
 // import sqlServer from "./data/connectors/mssql.connector";
 
 const GRAPHQL_PORT = 3010;
 
 const graphQLServer = express();
 
+graphQLServer.use(cors());
 graphQLServer.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
-
-graphQLServer.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 graphQLServer.listen(process.env.PORT || GRAPHQL_PORT, () =>
   console.log(
