9bf340e4404501debea6e47240531424035a8374
Sarasate <sarasate@users.noreply.github.com>
2017-07-24 10:34:00 +0200

    Use dsteem form `comment()`

--- data/comment/comment.resolver.js
+++ data/comment/comment.resolver.js
@@ -6,34 +6,36 @@ import moment from "moment";
 
 const CommentResolvers = {
   Mutation: {
-    async comment(root, args) {
-      const { comment, key } = args;
-      console.log(comment);
-      const res = await steem.broadcast.commentAsync(
-        key,
-        comment.parent_author,
-        comment.parent_permlink,
-        comment.author,
-        comment.permlink,
-        comment.title,
-        comment.body,
-        comment.json_metadata
-      );
-      console.log(res);
-      return res;
-    },
+    // async comment(root, args) {
+    //   const { comment, key } = args;
+    //   const res = await steem.broadcast.commentAsync(
+    //     key,
+    //     comment.parent_author,
+    //     comment.parent_permlink,
+    //     comment.author,
+    //     comment.permlink,
+    //     comment.title,
+    //     comment.body,
+    //     comment.json_metadata
+    //   );
+    //   console.log(res)
+    //   return res;
+    // },
 
     async deleteComment(root, args) {
       const { key, author, permlink } = args;
       return steem.broadcast.deleteComment(key, author, permlink);
+    },
+
+    async comment(root, args) {
+      let { comment, key } = args;
+      const result = await dsteem.broadcast.comment(
+        comment,
+        PrivateKey.from(key)
+      );
+      console.log(result);
+      return result;
     }
-    // async comment(root, args) {
-    //   let { comment, key } = args;
-    //   key = PrivateKey.fromString(key);
-    //   console.log(key.toString());
-    //   const result = await dsteem.broadcast.comment(comment, key);
-    //   return result;
-    // }
   }
 };
 
--- data/connectors/dsteem.connector.js
+++ data/connectors/dsteem.connector.js
@@ -5,6 +5,9 @@ import { config } from "dotenv";
 config();
 
 // const client = new Client("wss://steemd.steemit.com");
-const client = new Client(process.env.STEEM_WEBSOCKET_URL);
+const client = new Client(process.env.STEEM_WEBSOCKET_URL, {
+  chainId: "79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673",
+  addressPrefix: "STX"
+});
 
 export default client;
