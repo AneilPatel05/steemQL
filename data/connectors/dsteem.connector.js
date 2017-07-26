6637f09680f23783bf6b8929cbaa51c756e0e0bc
Sarasate <sarasate@users.noreply.github.com>
2017-07-26 21:14:23 +0200

    Fix dsteem config

--- data/connectors/dsteem.connector.js
+++ data/connectors/dsteem.connector.js
@@ -6,8 +6,8 @@ config();
 
 // const client = new Client("wss://steemd.steemit.com");
 const client = new Client(process.env.STEEM_WEBSOCKET_URL, {
-  chainId: "79276aea5d4877d9a25892eaa01b0adf019d3e5cb12a97478df3298ccdd01673",
-  addressPrefix: "STX"
+  chainId: process.env.STEEM_CHAIN_ID,
+  addressPrefix: process.env.STEEM_ADDRESS_PREFIX
 });
 
 export default client;
