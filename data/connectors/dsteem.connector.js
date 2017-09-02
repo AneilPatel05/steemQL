ac59da2f3422c397f6bf3a27b4de888cfdc8f34b
sarasate <luis@superlumen.io>
2017-09-02 11:44:34 +0200

    Reset dsteem config

--- data/connectors/dsteem.connector.js
+++ data/connectors/dsteem.connector.js
@@ -4,10 +4,10 @@ import { config } from "dotenv";
 // Execute .env config.
 config();
 
-const client = new Client("wss://steemd.steemit.com");
-// const client = new Client(process.env.STEEM_WEBSOCKET_URL, {
-//   chainId: process.env.STEEM_CHAIN_ID,
-//   addressPrefix: process.env.STEEM_ADDRESS_PREFIX
-// });
+// const client = new Client("wss://steemd.steemit.com");
+const client = new Client(process.env.STEEM_WEBSOCKET_URL, {
+  chainId: process.env.STEEM_CHAIN_ID,
+  addressPrefix: process.env.STEEM_ADDRESS_PREFIX
+});
 
 export default client;
